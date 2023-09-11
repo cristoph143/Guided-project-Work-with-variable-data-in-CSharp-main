8b273cc7c2cb2514d4f3948d448e8b39ff6db3e3
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-11 11:19:01 +0800

    Exercise - Add improved search animation

--- Program.cs
+++ Program.cs
@@ -121,41 +121,94 @@ do
 
             break;
 
-        case "2":
-            // Display all dogs with a specified characteristic
-            string dogCharacteristic = "";
+         case "2":
+            // #1 Display all dogs with a multiple search characteristics
 
-            while (dogCharacteristic == "")
+            string dogCharacteristics = "";
+
+            while (dogCharacteristics == "")
             {
-                // have the user enter physical characteristics to search for
-                Console.WriteLine($"\nEnter one desired dog characteristics to search for");
+                // #2 have user enter multiple comma separated characteristics to search for
+                Console.WriteLine($"\nEnter dog characteristics to search for separated by commas");
                 readResult = Console.ReadLine();
+
                 if (readResult != null)
                 {
-                    dogCharacteristic = readResult.ToLower().Trim();
+                    dogCharacteristics = readResult.ToLower();
+                    Console.WriteLine();
                 }
-            } 
-            // #6 loop through the ourAnimals array to search for matching animals
+            }
+
+            string[] dogSearches = dogCharacteristics.Split(",");
+            // trim leading and trailing spaces from each search term
+            for (int i = 0; i < dogSearches.Length; i++)
+            {
+                dogSearches[i] = dogSearches[i].Trim();
+            }
+
+            Array.Sort(dogSearches);
+            // #4 update to "rotating" animation with countdown
+            string[] searchingIcons = {" |", " /", "--", " \\", " *"};
+
+            bool matchesAnyDog = false;
+            string dogDescription = "";
+
+            // loops through the ourAnimals array to search for matching animals
             for (int i = 0; i < maxPets; i++)
             {
                 if (ourAnimals[i, 1].Contains("dog"))
                 {
-                    // #7 Search combined descriptions and report results
-                    string dogDescription = "";
+
+                    // Search combined descriptions and report results
                     dogDescription = ourAnimals[i, 4] + "\n" + ourAnimals[i, 5];
-                    bool noMatchesDog = true;
-                    if (dogDescription.Contains(dogCharacteristic))
+                    bool matchesCurrentDog = false;
+
+                    foreach (string term in dogSearches)
                     {
-                        Console.WriteLine($"\nOur dog {ourAnimals[i, 3]} is a match!");
-                        Console.WriteLine(dogDescription);
-                        noMatchesDog = false;
+                        // only search if there is a term to search for
+                        if (term != null && term.Trim() != "")
+                        {
+                            for (int j = 2; j > -1 ; j--)
+                            {
+                                // #5 update "searching" message to show countdown
+                                foreach (string icon in searchingIcons)
+                                {
+                                    Console.Write($"\rsearching our dog {ourAnimals[i, 3]} for {term.Trim()} {icon} {j.ToString()}");
+                                    Thread.Sleep(100);
+                                }
+                                
+                                Console.Write($"\r{new String(' ', Console.BufferWidth)}");
+                            }
+
+                            // #3a iterate submitted characteristic terms and search description for each term
+                            if (dogDescription.Contains(" " + term.Trim() + " "))
+                            {
+                                // #3b update message to reflect current search term match 
+
+                                Console.WriteLine($"\rOur dog {ourAnimals[i, 3]} matches your search for {term.Trim()}");
+
+                                matchesCurrentDog = true;
+                                matchesAnyDog = true;
+                            }
+                        }
                     }
-                    if (noMatchesDog)
+                    
+                    // #3d if the current dog is match, display the dog's info
+                    if (matchesCurrentDog)
                     {
-                        Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristic);
+                        Console.WriteLine($"\r{ourAnimals[i, 3]} ({ourAnimals[i, 0]})\n{dogDescription}\n");
                     }
                 }
             }
+
+            if (!matchesAnyDog)
+            {
+                Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristics);
+            }
+
+            Console.WriteLine("\n\rPress the Enter key to continue");
+            readResult = Console.ReadLine();
+
             break;
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # Starter
+
+How to check your work
+To validate that your code satisfies the specified requirements, complete the following steps:
+
+Use Visual Studio Code to build and run your app.
+ Note
+
+You can exit the verification test before completing all of the verification steps if see a result that does not satisfy the specification requirements. To force an exit from the running program, in the Terminal panel, press Ctrl-C. After exiting the running app, complete the edits that you believe will address the issue you are working on, save your updates to the Program.cs file, and then re-build and run your code.
+
+The terminal command prompt should display as the starting point for the program
+
+At the command prompt, enter 2 menu:
+
+Output
+
+Copy
+Welcome to the Contoso PetFriends app. Your main menu options are:
+ 1. List all of our current pet information
+ 2. Display all dogs with a specified characteristic
+
+Enter your selection number (or type Exit to exit the program)
+2
+
+Enter dog characteristics to search for separated by commas
+At the command prompt, enter large, cream, golden to test when more than one search term matches the dog descriptions. Verify that the Terminal panel updates with a message similar to the code output sample:
+
+Output
+
+Copy
+Enter dog characteristics to search for separated by commas
+large, cream, golden
+
+Our dog Nickname: lola matches your search for cream
+Our dog Nickname: lola matches your search for golden
+Nickname: lola (ID #: d1)
+Physical description: medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.
+Personality: loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.
+
+Our dog Nickname: gus matches your search for golden
+Our dog Nickname: gus matches your search for large
+Nickname: gus (ID #: d2)
+Physical description: large reddish-brown male golden retriever weighing about 85 pounds. housebroken.
+Personality: loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.
+
+Press the Enter key to continue
+At the command prompt, press the enter key to continue to the main menu
+
+At the command prompt, enter 2 menu:
+
+Output
+
+Copy
+Welcome to the Contoso PetFriends app. Your main menu options are:
+ 1. List all of our current pet information
+ 2. Display all dogs with a specified characteristic
+
+Enter your selection number (or type Exit to exit the program)
+2
+
+Enter dog characteristics to search for separated by commas
+
+At the command prompt, enter big, grey, stripes to test when none of the search terms match dog descriptions. Verify that the Terminal panel updates with a message similar to the code output samples:
+
+Output
+
+Copy
+Enter dog characteristics to search for separated by commas
+big, grey, stripes
+
+None of our dogs are a match for: big, grey, stripes
+
+
+Press the Enter key to continue
+If you specified further restrictions for valid entries, run the appropriate test cases to verify your work.
+
+ Note
+
+If your code meets the requirements you should be able to complete each step in order and see the expected results in a single test pass. If you added additional restrictions, you may need to exit the application and then run a separate test pass to complete your verification.
+
+Congratulations if you succeeded in this challenge exercise!
