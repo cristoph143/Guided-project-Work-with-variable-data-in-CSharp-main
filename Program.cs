5d7b56e00a1533725923398e000d60f9655a13f9
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-09-11 11:58:52 +0800

    refactor.jpg - been able to convert into one class program

--- Program.cs
+++ Program.cs
@@ -1,215 +1,238 @@
-﻿// #1 the ourAnimals array will store the following: 
-string animalSpecies = "";
-string animalID = "";
-string animalAge = "";
-string animalPhysicalDescription = "";
-string animalPersonalityDescription = "";
-string animalNickname = "";
-string suggestedDonation = "";
-// #2 variables that support data entry
-int maxPets = 8;
-string? readResult;
-string menuSelection = "";
-decimal decimalDonation = 0.00m;
-// #3 array used to store runtime data, there is no persisted data
-string[,] ourAnimals = new string[maxPets, 7];
+﻿namespace Starter;
 
-// #4 create sample data ourAnimals array entries
-for (int i = 0; i < maxPets; i++)
+internal abstract class Program
 {
-    switch (i)
+    public static void Main()
     {
-        case 0:
-            animalSpecies = "dog";
-            animalID = "d1";
-            animalAge = "2";
-            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
-            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
-            animalNickname = "lola";
-            suggestedDonation = "85.00";
-            break;
-
-        case 1:
-            animalSpecies = "dog";
-            animalID = "d2";
-            animalAge = "9";
-            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
-            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
-            animalNickname = "gus";
-            suggestedDonation = "49.99";
-            break;
-        
-        case 2:
-            animalSpecies = "cat";
-            animalID = "c3";
-            animalAge = "1";
-            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
-            animalPersonalityDescription = "friendly";
-            animalNickname = "snow";
-            suggestedDonation = "40.00";
-            break;
-
-        case 3:
-            animalSpecies = "cat";
-            animalID = "c4";
-            animalAge = "3";
-            animalPhysicalDescription = "Medium sized, long hair, yellow, female, about 10 pounds. Uses litter box.";
-            animalPersonalityDescription = "A people loving cat that likes to sit on your lap.";
-            animalNickname = "Lion";
-            suggestedDonation = "";
-            break;
-        
-        default:
-            animalSpecies = "";
-            animalID = "";
-            animalAge = "";
-            animalPhysicalDescription = "";
-            animalPersonalityDescription = "";
-            animalNickname = "";
-            break;
-
+        const string suggestedDonation = "";
+        // #2 variables that support data entry
+        const int maxPets = 8;
+        // #3 array used to store runtime data, there is no persisted data
+        var ourAnimals = new string[maxPets, 7];
+        // #4 create sample data ourAnimals array entries
+        SamplePetData(maxPets, suggestedDonation, ourAnimals);
+        MainMenu(maxPets, ourAnimals);
     }
 
-    ourAnimals[i, 0] = "ID #: " + animalID;
-    ourAnimals[i, 1] = "Species: " + animalSpecies;
-    ourAnimals[i, 2] = "Age: " + animalAge;
-    ourAnimals[i, 3] = "Nickname: " + animalNickname;
-    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
-    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
-    if (!decimal.TryParse(suggestedDonation, out decimalDonation)){
-        decimalDonation = 45.00m; // if suggestedDonation NOT a number, default to 45.00
+    private static void SamplePetData(int maxPets1, string s, string[,] strings)
+    {
+        for (var i = 0; i < maxPets1; i++)
+        {
+            string animalSpecies1;
+            string animalId;
+            string animalAge1;
+            string animalPhysicalDescription1;
+            string animalPersonalityDescription1;
+            string animalNickname1;
+            switch (i)
+            {
+                case 0:
+                    animalSpecies1 = "dog";
+                    animalId = "d1";
+                    animalAge1 = "2";
+                    animalPhysicalDescription1 =
+                        "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
+                    animalPersonalityDescription1 =
+                        "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+                    animalNickname1 = "lola";
+                    s = "85.00";
+                    break;
+
+                case 1:
+                    animalSpecies1 = "dog";
+                    animalId = "d2";
+                    animalAge1 = "9";
+                    animalPhysicalDescription1 =
+                        "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+                    animalPersonalityDescription1 =
+                        "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+                    animalNickname1 = "gus";
+                    s = "49.99";
+                    break;
+
+                case 2:
+                    animalSpecies1 = "cat";
+                    animalId = "c3";
+                    animalAge1 = "1";
+                    animalPhysicalDescription1 = "small white female weighing about 8 pounds. litter box trained.";
+                    animalPersonalityDescription1 = "friendly";
+                    animalNickname1 = "snow";
+                    s = "40.00";
+                    break;
+
+                case 3:
+                    animalSpecies1 = "cat";
+                    animalId = "c4";
+                    animalAge1 = "3";
+                    animalPhysicalDescription1 =
+                        "Medium sized, long hair, yellow, female, about 10 pounds. Uses litter box.";
+                    animalPersonalityDescription1 = "A people loving cat that likes to sit on your lap.";
+                    animalNickname1 = "Lion";
+                    s = "";
+                    break;
+
+                default:
+                    animalSpecies1 = "";
+                    animalId = "";
+                    animalAge1 = "";
+                    animalPhysicalDescription1 = "";
+                    animalPersonalityDescription1 = "";
+                    animalNickname1 = "";
+                    break;
+            }
+
+            strings[i, 0] = "ID #: " + animalId;
+            strings[i, 1] = "Species: " + animalSpecies1;
+            strings[i, 2] = "Age: " + animalAge1;
+            strings[i, 3] = "Nickname: " + animalNickname1;
+            strings[i, 4] = "Physical description: " + animalPhysicalDescription1;
+            strings[i, 5] = "Personality: " + animalPersonalityDescription1;
+            if (!decimal.TryParse(s, out var decimalDonation1))
+                decimalDonation1 = 45.00m; // if suggestedDonation NOT a number, default to 45.00
+
+            strings[i, 6] = $"Suggested Donation: {decimalDonation1:C2}";
+        }
     }
-    ourAnimals[i, 6] = $"Suggested Donation: {decimalDonation:C2}";
+
+    private static void MainMenu(int maxPets, string[,] ourAnimals)
+    {
+        var menuSelection = "";
+        // #5 display the top-level menu options
+        do
+        {
+            DisplayFunction();
+
+            var readResult = Console.ReadLine();
+            if (readResult != null) menuSelection = readResult.ToLower();
+
+            MenuSelection(maxPets, ourAnimals, menuSelection);
+        } while (menuSelection != "exit");
+    }
+
+    private static string? MenuSelection(int maxPets1, string[,] strings, string s)
+    {
+        // use switch-case to process the selected menu option
+        return s switch
+        {
+            "1" => DisplayListOfDogs(maxPets1, strings),
+            "2" => DisplaySearchDogResult(maxPets1, strings),
+            _ => null
+        };
+    }
+
+    private static string GetDogCharacteristics()
+    {
+        var dogCharacteristics = "";
+        while (dogCharacteristics == "")
+        {
+            // #2 have user enter multiple comma separated characteristics to search for
+            Console.WriteLine("\nEnter dog characteristics to search for separated by commas");
+            var readResult = Console.ReadLine();
+
+            if (readResult == null) continue;
+            dogCharacteristics = readResult.ToLower();
+            Console.WriteLine();
+        }
+
+        return dogCharacteristics;
+    }
+
+    private static string? DisplaySearchDogResult(int maxPets1, string[,] strings)
+    {
+        // #1 Display all dogs with a multiple search characteristics
+        var dogCharacteristics = GetDogCharacteristics();
+        var dogSearches = dogCharacteristics.Split(",");
+        // trim leading and trailing spaces from each search term
+        for (var i = 0; i < dogSearches.Length; i++) dogSearches[i] = dogSearches[i].Trim();
+
+        Array.Sort(dogSearches);
+        // #4 update to "rotating" animation with countdown
+        string[] searchingIcons = { " |", " /", "--", " \\", " *" };
+
+        var matchesAnyDog = false;
+        // loops through the ourAnimals array to search for matching animals
+        for (var i = 0; i < maxPets1; i++) matchesAnyDog = MatchesAnyDog(strings, i, dogSearches, searchingIcons, matchesAnyDog);
+
+        if (!matchesAnyDog)
+            Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristics);
+
+        Console.WriteLine("\n\rPress the Enter key to continue");
+        var readResult = Console.ReadLine();
+        return readResult;
+    }
+
+    private static bool MatchesAnyDog(string[,] strings1, int i, string[] dogSearches1, string[] searchingIcons1, bool matchesAnyDog)
+        {
+            if (!strings1[i, 1].Contains("dog")) return matchesAnyDog;
+            // Search combined descriptions and report results
+            var dogDescription = strings1[i, 4] + "\n" + strings1[i, 5];
+            var matchesCurrentDog = false;
+
+            foreach (var term in dogSearches1)
+                // only search if there is a term to search for
+                (matchesAnyDog, matchesCurrentDog) = MatchesAnyDogTerm(strings1, i, searchingIcons1, term, dogDescription, matchesCurrentDog);
+
+            // #3d if the current dog is match, display the dog's info
+            if (matchesCurrentDog)
+                Console.WriteLine($"\r{strings1[i, 3]} ({strings1[i, 0]})\n{dogDescription}\n");
+
+            return matchesAnyDog;
+        }
+
+    private static (bool matchesAnyDog, bool matchesCurrentDog) MatchesAnyDogTerm(string[,] strings1, int i,
+        string[] searchingIcons1, string term, string dogDescription, bool matchesCurrentDog)
+    {
+        bool matchesAnyDog = false;
+        if (term == null || term.Trim() == "") return (matchesAnyDog, matchesCurrentDog);
+        for (var j = 2; j > -1; j--)
+        {
+            // #5 update "searching" message to show countdown
+            foreach (var icon in searchingIcons1)
+            {
+                Console.Write(
+                    $"\rsearching our dog {strings1[i, 3]} for {term.Trim()} {icon} {j.ToString()}");
+                Thread.Sleep(100);
+            }
+
+            Console.Write($"\r{new string(' ', Console.BufferWidth)}");
+        }
+
+        // #3a iterate submitted characteristic terms and search description for each term
+        if (!dogDescription.Contains(" " + term.Trim() + " ")) return (matchesAnyDog, matchesCurrentDog);
+        // #3b update message to reflect current search term match 
+        Console.WriteLine(
+            $"\rOur dog {strings1[i, 3]} matches your search for {term.Trim()}");
+
+        matchesCurrentDog = true;
+        matchesAnyDog = true;
+
+        return (matchesAnyDog, matchesCurrentDog);
+    }
+
+    private static string? DisplayListOfDogs(int maxPets1, string[,] strings)
+    {
+        // list all pet info
+        for (var i = 0; i < maxPets1; i++)
+            if (strings[i, 0] != "ID #: ")
+            {
+                Console.WriteLine();
+                for (var j = 0; j < 7; j++) // increased exit condition
+                    Console.WriteLine(strings[i, j]);
+            }
+
+        Console.WriteLine("\n\rPress the Enter key to continue");
+        var readResult = Console.ReadLine();
+        return readResult;
+    }
+
+    private static void DisplayFunction()
+    {
+        // NOTE: the Console.Clear method is throwing an exception in debug sessions
+        Console.Clear();
+        Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+        Console.WriteLine(" 1. List all of our current pet information");
+        Console.WriteLine(" 2. Display all dogs with a specified characteristic");
+        Console.WriteLine();
+        Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+    }
 }
-
-// #5 display the top-level menu options
-do
-{
-    // NOTE: the Console.Clear method is throwing an exception in debug sessions
-    Console.Clear();
-
-    Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
-    Console.WriteLine(" 1. List all of our current pet information");
-    Console.WriteLine(" 2. Display all dogs with a specified characteristic");
-    Console.WriteLine();
-    Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
-
-    readResult = Console.ReadLine();
-    if (readResult != null)
-    {
-        menuSelection = readResult.ToLower();
-    }
-
-    // use switch-case to process the selected menu option
-    switch (menuSelection)
-    {
-        case "1":
-            // list all pet info
-            for (int i = 0; i < maxPets; i++)
-            {
-                if (ourAnimals[i, 0] != "ID #: ")
-                {
-                    Console.WriteLine();
-                    for (int j = 0; j < 7; j++) // increased exit condition
-                    {
-                        Console.WriteLine(ourAnimals[i, j]);
-                    }
-                }
-            }
-            Console.WriteLine("\n\rPress the Enter key to continue");
-            readResult = Console.ReadLine();
-
-            break;
-
-         case "2":
-            // #1 Display all dogs with a multiple search characteristics
-
-            string dogCharacteristics = "";
-
-            while (dogCharacteristics == "")
-            {
-                // #2 have user enter multiple comma separated characteristics to search for
-                Console.WriteLine($"\nEnter dog characteristics to search for separated by commas");
-                readResult = Console.ReadLine();
-
-                if (readResult != null)
-                {
-                    dogCharacteristics = readResult.ToLower();
-                    Console.WriteLine();
-                }
-            }
-
-            string[] dogSearches = dogCharacteristics.Split(",");
-            // trim leading and trailing spaces from each search term
-            for (int i = 0; i < dogSearches.Length; i++)
-            {
-                dogSearches[i] = dogSearches[i].Trim();
-            }
-
-            Array.Sort(dogSearches);
-            // #4 update to "rotating" animation with countdown
-            string[] searchingIcons = {" |", " /", "--", " \\", " *"};
-
-            bool matchesAnyDog = false;
-            string dogDescription = "";
-
-            // loops through the ourAnimals array to search for matching animals
-            for (int i = 0; i < maxPets; i++)
-            {
-                if (ourAnimals[i, 1].Contains("dog"))
-                {
-
-                    // Search combined descriptions and report results
-                    dogDescription = ourAnimals[i, 4] + "\n" + ourAnimals[i, 5];
-                    bool matchesCurrentDog = false;
-
-                    foreach (string term in dogSearches)
-                    {
-                        // only search if there is a term to search for
-                        if (term != null && term.Trim() != "")
-                        {
-                            for (int j = 2; j > -1 ; j--)
-                            {
-                                // #5 update "searching" message to show countdown
-                                foreach (string icon in searchingIcons)
-                                {
-                                    Console.Write($"\rsearching our dog {ourAnimals[i, 3]} for {term.Trim()} {icon} {j.ToString()}");
-                                    Thread.Sleep(100);
-                                }
-                                
-                                Console.Write($"\r{new String(' ', Console.BufferWidth)}");
-                            }
-
-                            // #3a iterate submitted characteristic terms and search description for each term
-                            if (dogDescription.Contains(" " + term.Trim() + " "))
-                            {
-                                // #3b update message to reflect current search term match 
-
-                                Console.WriteLine($"\rOur dog {ourAnimals[i, 3]} matches your search for {term.Trim()}");
-
-                                matchesCurrentDog = true;
-                                matchesAnyDog = true;
-                            }
-                        }
-                    }
-                    
-                    // #3d if the current dog is match, display the dog's info
-                    if (matchesCurrentDog)
-                    {
-                        Console.WriteLine($"\r{ourAnimals[i, 3]} ({ourAnimals[i, 0]})\n{dogDescription}\n");
-                    }
-                }
-            }
-
-            if (!matchesAnyDog)
-            {
-                Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristics);
-            }
-
-            Console.WriteLine("\n\rPress the Enter key to continue");
-            readResult = Console.ReadLine();
-
-            break;
-    }
-
-} while (menuSelection != "exit");
--- README.md
+++ README.md
@@ -4,9 +4,12 @@ How to check your work
 To validate that your code satisfies the specified requirements, complete the following steps:
 
 Use Visual Studio Code to build and run your app.
- Note
+Note
 
-You can exit the verification test before completing all of the verification steps if see a result that does not satisfy the specification requirements. To force an exit from the running program, in the Terminal panel, press Ctrl-C. After exiting the running app, complete the edits that you believe will address the issue you are working on, save your updates to the Program.cs file, and then re-build and run your code.
+You can exit the verification test before completing all of the verification steps if see a result that does not satisfy
+the specification requirements. To force an exit from the running program, in the Terminal panel, press Ctrl-C. After
+exiting the running app, complete the edits that you believe will address the issue you are working on, save your
+updates to the Program.cs file, and then re-build and run your code.
 
 The terminal command prompt should display as the starting point for the program
 
@@ -16,14 +19,16 @@ Output
 
 Copy
 Welcome to the Contoso PetFriends app. Your main menu options are:
- 1. List all of our current pet information
- 2. Display all dogs with a specified characteristic
+
+1. List all of our current pet information
+2. Display all dogs with a specified characteristic
 
 Enter your selection number (or type Exit to exit the program)
 2
 
 Enter dog characteristics to search for separated by commas
-At the command prompt, enter large, cream, golden to test when more than one search term matches the dog descriptions. Verify that the Terminal panel updates with a message similar to the code output sample:
+At the command prompt, enter large, cream, golden to test when more than one search term matches the dog descriptions.
+Verify that the Terminal panel updates with a message similar to the code output sample:
 
 Output
 
@@ -41,7 +46,8 @@ Our dog Nickname: gus matches your search for golden
 Our dog Nickname: gus matches your search for large
 Nickname: gus (ID #: d2)
 Physical description: large reddish-brown male golden retriever weighing about 85 pounds. housebroken.
-Personality: loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.
+Personality: loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give
+doggy hugs.
 
 Press the Enter key to continue
 At the command prompt, press the enter key to continue to the main menu
@@ -52,15 +58,17 @@ Output
 
 Copy
 Welcome to the Contoso PetFriends app. Your main menu options are:
- 1. List all of our current pet information
- 2. Display all dogs with a specified characteristic
+
+1. List all of our current pet information
+2. Display all dogs with a specified characteristic
 
 Enter your selection number (or type Exit to exit the program)
 2
 
 Enter dog characteristics to search for separated by commas
 
-At the command prompt, enter big, grey, stripes to test when none of the search terms match dog descriptions. Verify that the Terminal panel updates with a message similar to the code output samples:
+At the command prompt, enter big, grey, stripes to test when none of the search terms match dog descriptions. Verify
+that the Terminal panel updates with a message similar to the code output samples:
 
 Output
 
@@ -70,12 +78,13 @@ big, grey, stripes
 
 None of our dogs are a match for: big, grey, stripes
 
-
 Press the Enter key to continue
 If you specified further restrictions for valid entries, run the appropriate test cases to verify your work.
 
- Note
+Note
 
-If your code meets the requirements you should be able to complete each step in order and see the expected results in a single test pass. If you added additional restrictions, you may need to exit the application and then run a separate test pass to complete your verification.
+If your code meets the requirements you should be able to complete each step in order and see the expected results in a
+single test pass. If you added additional restrictions, you may need to exit the application and then run a separate
+test pass to complete your verification.
 
 Congratulations if you succeeded in this challenge exercise!
